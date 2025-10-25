a5e08c37f9347b6a92759b69c583df183b30a416
M Adeel <cmkali2126@gmail.com>
2025-10-25 15:14:04 +0500

    Create README.md

--- app/src/main/java/com/example/firebase_auth/third.java
+++ app/src/main/java/com/example/firebase_auth/third.java
@@ -1,6 +1,9 @@
 package com.example.firebase_auth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class third extends AppCompatActivity {
+
+    Button btn;
+    FirebaseAuth auth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_third);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+     btn=findViewById(R.id.button);
+     auth=FirebaseAuth.getInstance();
+     user=auth.getCurrentUser();
+     if(user==null){
+         Intent intent = new Intent(getApplicationContext(), login.class);
+         startActivity(intent);
+         finish();
+
+     }
+     btn.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             FirebaseAuth.getInstance()
+                     .signOut();
+             Intent intent = new Intent(getApplicationContext(), login.class);
+             startActivity(intent);
+             finish();
+         }
+     });
+
+
     }
 }
